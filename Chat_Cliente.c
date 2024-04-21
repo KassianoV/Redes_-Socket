1652c1f292d4946daaa7c59bf896ce876d756fc6
Kassiano Vieira <kassianovieira@id.uff.br>
2024-04-21 12:15:15 -0300

    atualização

--- Chat_Cliente.c
+++ Chat_Cliente.c
@@ -6,12 +6,15 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
-
 int main(){
   int meuSocket;
   int conecta;
   struct sockaddr_in server;
   meuSocket = socket(AF_INET, SOCK_STREAM, 0);
   
-  serveer.sin_family = AF_INET;
+  server.sin_family = AF_INET;
+  server.sin_port = htons(PORT);
+  server.sin_addr.s_addr = inet_addr("192.168.0.1");
+  
+  conecta = connect(meusocket,(struct sockaddr*)&server,sizeof server);
 }
