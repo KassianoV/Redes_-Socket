25011b21596cbedbff74de031105366809aee57e
Kassiano Vieira <kassianovieira@id.uff.br>
2024-04-21 13:00:39 -0300

    atualização os dois códigos

--- Chat_Cliente.c
+++ Chat_Cliente.c
@@ -5,16 +5,45 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 8080
+#define PORT 80
 int main(){
   int meuSocket;
   int conecta;
+  char buffer[1024];
   struct sockaddr_in server;
-  meuSocket = socket(AF_INET, SOCK_STREAM, 0);
   
+  meuSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(meuSocket < 0){
+    perror("ERRO AO CRIAR SOCKET");
+    exit(EXIT_FAILURE);
+  }
+  
+  //Configuração do endereço do servidor 
+  menset(&server, 0, sizeof(server));
   server.sin_family = AF_INET;
   server.sin_port = htons(PORT);
   server.sin_addr.s_addr = inet_addr("192.168.0.1");
   
+  //Conexão ao servidor
   conecta = connect(meusocket,(struct sockaddr*)&server,sizeof server);
+  if(conecta < 0){
+    perror("ERRO AO CONECTAR-SE AO SERVIDOR");
+    exit(EXIT_FAILURE);
+  }else{
+    
+  } 
+  
+  //Envio da mensagem de registro
+  strcpy(buffer,"quero me registrar");
+  send(meuSocket, buffer, strlen(buffer), 0);
+  printf("mensagem enviada: %s\n",buffer);
+  
+  //Recebimento da mensagem do servidor 
+  menset(buffer, 0, sizeof(buffer));
+  recv(meuSOcket, buffer, sizeof(buffer), 0);
+  printf("Resposta do servidor %s\n", buffer);
+  
+  //Fechamento sockect
+  close(meuSocket);
+  close(conecta);
 }
